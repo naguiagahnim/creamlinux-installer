9144e52db10a8f64002e4a1fc1cd64b54b546b43
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2026-05-05 16:59:22 +0200

    remove shell script
    Will be integrated directly into CI/CD

--- update-npm-hash.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-hash=$(nix-shell -p prefetch-npm-deps --run "prefetch-npm-deps package-lock.json" 2>/dev/null)
-echo "New hash: $hash"
-sed -i "s|hash = \"[^\"]*\"|hash = \"$hash\"|" package.nix
